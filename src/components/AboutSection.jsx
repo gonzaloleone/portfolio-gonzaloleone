4b5bdd04102b5490aed619e41ff7accc07c9cb0b
Gonzalo <gonzaleone03@gmail.com>
2026-04-15 11:53:16 -0300

    actualizacion CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/media/Gonzalo Leone-CV1.pdf"
+                href="/media/Gonzalo-Leone-CV1.pdf"
                 download="Gonzalo-Leone-CV1.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
